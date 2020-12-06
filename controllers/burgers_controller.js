58f8442d68c0b9384a1c2c690198d513102f662b
Irene Asay <irene.hsu.asay@gmail.com>
2020-12-06 11:57:38 +0800

    consolidate variables

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,37 +3,34 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var string = JSON.stringify(data); 
-    var burger = JSON.parse(string)
-    var hbsObject = {
-      burgers: burger
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+router.get("/", function (req, res) {
+  burger.all(function (data) {
+    var string = JSON.stringify(data);
+    var burger = JSON.parse(string);
+    console.log(burger)
+    res.render("index", { burgers: burger });
   });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/api/burgers", function (req, res) {
   burger.create([
     "burger_name", "devoured"
   ], [
     req.body.name, 0
-  ], function(result) {
+  ], function (result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
     devoured: req.body.devoured
-  }, condition, function(result) {
+  }, condition, function (result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
